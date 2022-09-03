14cdc25c742370aa19c38203caa0489fb597fb93
matdubuisson <matteo.dubuisson@student.uclouvain.be>
2022-09-03 15:58:03 +0200

    Add client GUI

--- app/about_screener/executable.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while [[ 1 ]]; do
-    read -p "test ==> " msg
-    echo $msg
-    sleep 1
-    echo "Now !!"
-done
--- normal_server_base/execute.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -Xmx2G -Xms2G -jar server.jar nogui
